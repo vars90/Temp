9d614fd06107a4c7a6cd93cbfc7a03fdb67d4942
Julien Vanier <julien@particle.io>
2019-08-27 14:42:02 -0400

    Change style of char*

--- test/test_acronym.c
+++ test/test_acronym.c
@@ -11,9 +11,9 @@ void tearDown(void)
 {
 }
 
-static void test_abbreviation(char *phrase, char *expected)
+static void test_abbreviation(char* phrase, char* expected)
 {
-   char *actual = abbreviate(phrase);
+   char* actual = abbreviate(phrase);
    TEST_ASSERT_EQUAL_STRING(expected, actual);
    free(actual);
 }
@@ -21,81 +21,81 @@ static void test_abbreviation(char *phrase, char *expected)
 static void test_null_string(void)
 {
    TEST_IGNORE();               // delete this line to run test
-   char *phrase = NULL;
-   char *expected = NULL;
+   char* phrase = NULL;
+   char* expected = NULL;
    test_abbreviation(phrase, expected);
 }
 
 static void test_basic_abbreviation(void)
 {
    TEST_IGNORE();
-   char *phrase = "Portable Network Graphics";
-   char *expected = "PNG";
+   char* phrase = "Portable Network Graphics";
+   char* expected = "PNG";
    test_abbreviation(phrase, expected);
 }
 
 static void test_lower_case_words(void)
 {
    TEST_IGNORE();
-   char *phrase = "Ruby on Rails";
-   char *expected = "ROR";
+   char* phrase = "Ruby on Rails";
+   char* expected = "ROR";
    test_abbreviation(phrase, expected);
 }
 
 static void test_punctuation(void)
 {
    TEST_IGNORE();
-   char *phrase = "First In, First Out";
-   char *expected = "FIFO";
+   char* phrase = "First In, First Out";
+   char* expected = "FIFO";
    test_abbreviation(phrase, expected);
 }
 
 static void test_non_acronym_all_caps_words(void)
 {
    TEST_IGNORE();
-   char *phrase = "GNU Image Manipulation Program";
-   char *expected = "GIMP";
+   char* phrase = "GNU Image Manipulation Program";
+   char* expected = "GIMP";
    test_abbreviation(phrase, expected);
 }
 
 static void test_hyphenated(void)
 {
    TEST_IGNORE();
-   char *phrase = "Complementary metal-oxide semiconductor";
-   char *expected = "CMOS";
+   char* phrase = "Complementary metal-oxide semiconductor";
+   char* expected = "CMOS";
    test_abbreviation(phrase, expected);
 }
 
 static void test_all_caps_words(void)
 {
    TEST_IGNORE();
-   char *phrase = "PHP: Hypertext Preprocessor";
-   char *expected = "PHP";
+   char* phrase = "PHP: Hypertext Preprocessor";
+   char* expected = "PHP";
    test_abbreviation(phrase, expected);
 }
 
 static void test_empty_string(void)
 {
    TEST_IGNORE();
-   char *phrase = "";
-   char *expected = NULL;
+   char* phrase = "";
+   char* expected = NULL;
    test_abbreviation(phrase, expected);
 }
 
 static void test_all_words_starting_with_lowercase(void)
 {
    TEST_IGNORE();
-   char *phrase = "for what it's worth";
-   char *expected = "FWIW";
+   char* phrase = "for what it's worth";
+   char* expected = "FWIW";
    test_abbreviation(phrase, expected);
 }
 
 static void test_long_abbreviation(void)
 {
    TEST_IGNORE();
-   char *phrase = "Rolling On The Floor Laughing So Hard "
+   char* phrase = "Rolling On The Floor Laughing So Hard "
        "That My Dogs Came Over And Licked Me";
-   char *expected = "ROTFLSHTMDCOALM";
+   char* expected = "ROTFLSHTMDCOALM";
    test_abbreviation(phrase, expected);
 }
 
