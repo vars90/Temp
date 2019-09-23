474f2eba94e5d26252ee70fd4bd6e264d8231d91
Varun-Chandra Reddy <vars90r@gmail.com>
2019-09-22 20:41:29 -0600

    Completed Abbreviate Function Ver1
    Need to test scenarios

--- src/acronym.c
+++ src/acronym.c
@@ -1,1 +1,41 @@
 #include "acronym.h"
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main()
+{
+    
+    char* phrase = "Portable,network Graphics";
+    char* expected = abbreviate(phrase);            // function call
+    
+    printf("%s in main function",expected);         // printing output
+    
+    return 0;
+}
+
+
+
+char* abbreviate(const char* phrase)
+{
+    static char expected[10];
+    int i=0;
+    int j=0;
+    expected[j]=toupper(phrase[i]);
+    
+    while(phrase[i]!='\0')                      // checking for end of string
+    {
+        if((phrase[i] == ' ') || (phrase[i] == ',') || (phrase[i] == '-'))                      // checking for space in the string
+        {
+            j++;
+            expected[j]=toupper(phrase[i+1]);            // geting the first character of the word
+        
+        }
+        i++;
+    }
+    
+    expected[j+1]='\0';                         // embedding end of string 
+    
+    printf("%s in the loop \t",expected);       // checking output in the function
+    return(expected);
+}
